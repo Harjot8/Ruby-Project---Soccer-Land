f347772c3a74ad1eab70a09bd9317d8e66e97dba
Harjot8 <harjot_8@live.ca>
2014-12-03 20:44:30 -0600

    Added a search function, using SQL statment

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,10 +1,18 @@
 class ProductController < ApplicationController
 	def index 
 		@products = Product.all
-		@category = Category.all
 	end 
 
 	def show
 		@product = Product.find(params[:id])
 	end
+
+	def search_results
+		#need to make an variable otherwise easy to be hacked
+		wildcard_keywords = '%' + params[:search_keywords] + '%'
+			
+		#SQL Statement using wildcard
+		@products = Product.where("name LIKE ?", wildcard_keywords)
+	end 
+	#view: app/views/store/search_result.html.erb
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'product#index'
 
   get 'product/:id' => 'product#show', constraints: {id: /\d+/}, as: 'product'
+  get 'search_results' => 'product#search_results', as: 'search_results'
+
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
