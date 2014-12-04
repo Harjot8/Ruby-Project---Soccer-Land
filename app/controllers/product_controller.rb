3605d83ada5cc6c9936605d1e457a0f7e4dc2114
Harjot8 <harjot_8@live.ca>
2014-12-03 21:59:00 -0600

    Added Favorite functionality to the project

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -1,6 +1,17 @@
 class ProductController < ApplicationController
+	before_action :load_favourite_product, except: :save_favourite_product
+
 	def index 
 		@products = Product.all
+		@category = Category.all
+
+		if session[:visit_count]
+			@visit_count = session[:visit_count] + 1
+		else 
+			@visit_count = 1
+		end
+
+		session[:visit_count] = @visit_count
 	end 
 
 	def show
@@ -15,4 +26,18 @@ class ProductController < ApplicationController
 		@products = Product.where("name LIKE ?", wildcard_keywords)
 	end 
 	#view: app/views/store/search_result.html.erb
+
+	def save_favourite_product
+		session[:favourite_product_id] = params[:id].to_i
+		redirect_to :back
+	end 
+
+	private
+	def load_favourite_product
+		if session[:favourite_product_id]
+			@favourite_product = Product.find(session[:favourite_product_id])
+		else 
+			@favourite_product = false
+		end 
+	end 
 end
