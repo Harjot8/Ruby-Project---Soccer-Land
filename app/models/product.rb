3a527a0b1708958f2dc17892dbb3e0db0ddf5726
Harjot8 <harjot_8@live.ca>
2014-12-07 01:06:09 -0600

    Added more styles to the index/show page

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,5 @@ class Product < ActiveRecord::Base
 	
 	validates :name, :description, :stock_quantity, :price, presence: true
 	validates :name, uniqueness: true
-	validates :price, numericality: {greater_than_or_equal_to: 0.01}
-
-	def favourite_product?(session)
-		session[:favourite_product_id] == self.id
-	end 	
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}	
 end
