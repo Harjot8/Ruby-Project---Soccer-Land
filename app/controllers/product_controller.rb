e34b6d9938da83493eb70a740037ddd9345c39d6
Harjot8 <harjot_8@live.ca>
2014-12-09 13:40:00 -0600

    Added category_search_results action

--- app/controllers/product_controller.rb
+++ app/controllers/product_controller.rb
@@ -2,6 +2,7 @@ class ProductController < ApplicationController
 	before_action :load_favourite_product, except: :save_favourite_product
 
 	def index 
+		@categories = Category.all
 		@pages = Page.all
 		@products = Product.order("name").page(params[:page]).per(6)
 		@category = Category.all
@@ -15,28 +16,40 @@ class ProductController < ApplicationController
 		session[:visit_count] = @visit_count
 	end 
 
+
+	def category_search_results
+		@pages = Page.all
+		@categories = Category.all
+		wildcard_keywords = '%' + params[:search_keywords] + '%'
+		@products = Product.where("category_id = ? AND name LIKE ? OR description LIKE ?", params[:my_option], wildcard_keywords, wildcard_keywords).page(params[:page]).per(6)
+	end 
+
 	def show
+		@categories = Category.all
 		@pages = Page.all
 		@product = Product.find(params[:id])
 	end
 
 	def search_results
+		@categories = Category.all
 		@pages = Page.all
 		#need to make an variable otherwise easy to be hacked
 		wildcard_keywords = '%' + params[:search_keywords] + '%'
 			
 		#SQL Statement using wildcard
-		@products = Product.where("name LIKE ?", wildcard_keywords)
+		@products = Product.where("name LIKE ? OR description LIKE ?", wildcard_keywords, wildcard_keywords).order("name").page(params[:page]).per(6)
 	end 
 	#view: app/views/store/search_result.html.erb
 
 	def save_favourite_product
+		@categories = Category.all
 		session[:favourite_product_id] = params[:id].to_i
 		redirect_to :back
 	end 
 
 	private
 	def load_favourite_product
+		@categories = Category.all
 		if session[:favourite_product_id]
 			@favourite_product = Product.find(session[:favourite_product_id])
 		else 
