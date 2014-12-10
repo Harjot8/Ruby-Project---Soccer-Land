271a923a9a0a9eb51f46036285fa3007f57aca01
Harjot8 <harjot_8@live.ca>
2014-12-10 09:31:25 -0600

    Added new HTML 5 Tags within

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,6 +1,7 @@
 class PageController < ApplicationController
 
 	def show
+		@categories = Category.all
 		@pages = Page.all
 		page_id = params[:id]
 		@page = Page.find(page_id)
@@ -8,11 +9,13 @@ class PageController < ApplicationController
 	#The associated view will load: app/views/pages/show.html.erb
 
 	def login
+		@categories = Category.all
 		@pages = Page.all
 		@customer = Customer.first
 	end 
 
 	def register
+		@categories = Category.all
 		@pages = Page.all
 		@customer = Customer.new
 	end 
